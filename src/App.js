d14c08c816e0ef1907be8aaf333ffa23b02c53bd
Datta Mundada <dbmundada333@gmail.com>
2021-07-14 22:08:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import ErrorBoundary from './components/ErrorBoundary';
 import MainPage from './components/MainPage';
 
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="serger";
+const stripeAcct = 12123;
+const username = "asfweferg.com";
+
 class App extends React.Component {
   render() {
     return (
