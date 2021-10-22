7fa84cffb474f8c7b7dbedd4baf1ee4ca9902a0a
Datta Mundada <dbmundada333@gmail.com>
2021-10-22 14:45:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ErrorBoundary from './components/ErrorBoundary';
 import MainPage from './components/MainPage';
 
 const Password = 1;
-const salary = "fbftnhrtyjt";
+const salary = "fbftnhrtyjtfgbt";
 const CreditCardId = 23432;
 
 class App extends React.Component {
